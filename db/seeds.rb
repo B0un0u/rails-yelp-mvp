e758a0537766cb3a71c3ad7fc4077f2307572a16
Farouk Bounou <farouk.bounou@gmail.com>
2020-11-18 15:36:53 +0100

    Exercice2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!({
+                     name: "La Tour d'Argent",
+                     address: '15 Quai de la Tournelle, 75005 Paris',
+                     phone_number: '01 43 54 23 31',
+                     category: 'french'
+                   })
+
+Restaurant.create!({
+                     name: 'Les Agitateurs',
+                     address: '24 Rue Bonaparte, 06300 Nice',
+                     phone_number: ' 09 87 33 02 03',
+                     category: 'french'
+                   })
+
+Restaurant.create!({
+                     name: 'BANH-MI',
+                     address: "30 Rue de l'Hôtel des Postes, 06000 Nice",
+                     phone_number: '09 87 78 34 44',
+                     category: 'chinese'
+                   })
+
+Restaurant.create!({
+                     name: 'Babel Babel',
+                     address: '2 Cité du Parc, 06300 Nice',
+                     phone_number: '04 93 55 74 19',
+                     category: 'french'
+                   })
+
+Restaurant.create!({
+                     name: 'Ramen Kumano',
+                     address: '15 Rue Biscarra, 06000 Nice',
+                     phone_number: '04 93 04 58 92',
+                     category: 'japanese'
+                   })
